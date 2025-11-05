f7f8296607acbd453ac693ee9c99f18d5d8ed291
dharm <dharm@Logesh_PC>
2025-11-06 01:14:46 +0530

    porject 8

---
+++ RestControl_RestfulApi/src/main/java/com/tnsif/restcontrol_restapi/RestControlRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.restcontrol_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControlRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControlRestfulApiApplication.class, args);
+	}
+
+}
---
+++ RestControl_RestfulApi/src/test/java/com/tnsif/restcontrol_restapi/RestControlRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.restcontrol_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControlRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
