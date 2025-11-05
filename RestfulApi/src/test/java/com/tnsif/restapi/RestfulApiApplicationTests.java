5546a891e7b116dbbb22f760b3861ec3f054a3ea
dharm <dharm@Logesh_PC>
2025-11-06 01:11:12 +0530

    project 7

--- RestfulApi/src/test/java/com/tnsif/restapi/RestfulApiApplicationTests.java
+++ RestfulApi/src/test/java/com/tnsif/restapi/RestfulApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tnsif.restapi_project7;
+package com.tnsif.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestfulApiProject7ApplicationTests {
+class RestfulApiApplicationTests {
 
 	@Test
 	void contextLoads() {
