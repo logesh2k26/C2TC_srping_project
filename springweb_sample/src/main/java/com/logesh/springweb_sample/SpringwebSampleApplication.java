cb9241013cfc8902f571a8137f9649507fc9f8b4
dharm <dharm@Logesh_PC>
2025-11-05 23:31:01 +0530

    commit

---
+++ springweb_sample/src/main/java/com/logesh/springweb_sample/SpringwebSampleApplication.java
@@ -0,0 +1,13 @@
+package com.logesh.springweb_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebSampleApplication.class, args);
+	}
+
+}
